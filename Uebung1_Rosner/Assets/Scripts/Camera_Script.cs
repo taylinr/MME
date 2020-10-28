fe0e1f4207cfee3904d1707207ef0bf70ee2c67d
Tanja Rosner <tr@pragma-shift.net>
2020-10-28 10:05:20 +0100

    initial setup

---
+++ Uebung1_Rosner/Assets/Scripts/Camera_Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Script : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public bool playerCoupling = true;
+
+    // Start is called before the first frame update
+    void Start () 
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+        
+    }
+
+    void LateUpdate () 
+    {
+         if (playerCoupling) {
+            Vector3 offsetRotated = player.transform.rotation * offset;
+            transform.position = player.transform.position + offsetRotated;
+         } 
+
+         Vector3 targetDirection = player.transform.position - transform.position;
+         transform.rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+    }
+}
---
+++ Uebung1_Rosner/Assets/Scripts/GameObject_Script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameObject_Script : MonoBehaviour
+{
+
+    public Transform myPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int objectCount = 5;
+
+        for(int i = 0; i < objectCount; i++){
+            Vector3 vector3 = new Vector3(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10));
+            Instantiate(myPrefab, vector3, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
+
+
