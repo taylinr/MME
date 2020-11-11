5f1a868562fc966eff997929c9015230217aae92
Tanja Rosner <tr@pragma-shift.net>
2020-11-11 23:51:34 +0100

    Added New Uebung

---
+++ Uebung2_Rosner/Assets/Scripts/MenuManager_Script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager_Script : MonoBehaviour
+{
+
+    public GameObject Menu;
+    public static bool IsPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+
+            if (IsPaused) {
+                Return();
+            } else {
+                Pause();
+            }
+
+        }
+    }
+
+    public void Return() { 
+        Menu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause() { 
+        Menu.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void Restart(){
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+        IsPaused = false;
+        Time.timeScale = 1f;
+    }
+}
